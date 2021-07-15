09133b30cf8cdefd5cc46a159d9fb894f4bfcd2b
Ivan Valles Perez <ivanvallesperez@gmail.com>
2021-07-15 22:06:04 +0100

    Add gpu control

--- batch.sh
+++ batch.sh
@@ -2,6 +2,8 @@
 set -e
 
 SEED=$1
+GPU=$2
+
 
 # cd to directory of this script
 cd "$(dirname "$0")"
@@ -10,16 +12,16 @@ cd "$(dirname "$0")"
 source .venv/bin/activate
 
 # Run experiment for cifar100 with all the models
-python main.py --model-name Conv6 --dataset-name cifar100 --seed $SEED
-python main.py --model-name Conv4 --dataset-name cifar100 --seed $SEED
-python main.py --model-name FC --dataset-name cifar100 --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name Conv6 --dataset-name cifar100 --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name Conv4 --dataset-name cifar100 --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name FC --dataset-name cifar100 --seed $SEED
 
 # Run experiment for cifar10 with all the models
-python main.py --model-name Conv6 --dataset-name cifar10 --seed $SEED
-python main.py --model-name Conv4 --dataset-name cifar10 --seed $SEED
-python main.py --model-name FC --dataset-name cifar10 --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name Conv6 --dataset-name cifar10 --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name Conv4 --dataset-name cifar10 --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name FC --dataset-name cifar10 --seed $SEED
 
 # Run experiment for fashion mnist with all the models
-python main.py --model-name Conv6 --dataset-name fmnist --seed $SEED
-python main.py --model-name Conv4 --dataset-name fmnist --seed $SEED
-python main.py --model-name FC --dataset-name fmnist --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name Conv6 --dataset-name fmnist --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name Conv4 --dataset-name fmnist --seed $SEED
+CUDA_VISIBLE_DEVICES=$GPU python main.py --model-name FC --dataset-name fmnist --seed $SEED
